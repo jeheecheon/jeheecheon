7e12b117b4a7bdb31b7af0d2bee86267d9547802
Jehee Cheon <jeheecheon@gmail.com>
2023-04-01 16:27:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Hi there 👋
+
+2023-04-01  
+I’m currently learning ASP.NET Core MVC and Razor Pages. On a path to become a back-end developer. I will test some learning stuff on [this repo](https://github.com/jeheecheon/jeheecheon) at first and will turn it into my personal blog where I can post things in public. Plans could change but, hopefully I will learn a lot along the way 😄
---
+++ jeheecheon/Data/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using jeheecheon.Models;
+
+namespace jeheecheon.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext (DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<jeheecheon.Models.Book> Book { get; set; } = default!;
+    }
+}
