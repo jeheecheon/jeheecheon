7b5b016d8a4ada1b939450d12d42d678201d12e4
Jehee Cheon <jeheecheon@gmail.com>
2023-08-20 19:20:42 +0900

    Move React Client content into src/ReactClient folder

---
+++ src/ReactClient/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <>
+      <h1>{number}</h1>
+      <button onClick={() => {
+        setNumber(n => {
+          alert(n);
+          return n + 1;
+        });
+        setNumber(n => {
+          alert(n)
+          return n + 1;
+        });
+        setNumber(n => {
+          alert(n)
+          return n + 1;
+        });
+        alert("triggered");
+      }}>+3</button>
+    </>
+  )
+}
+
+// import React, { Component } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import AppRoutes from './AppRoutes';
+// import { Layout } from './components/Layout';
+// import './custom.css';
+
+// export default class App extends Component {
+//   static displayName = App.name;
+
+//   render() {
+//     return (
+//       <Layout>
+//         <Routes>
+//           {AppRoutes.map((route, index) => {
+//             const { element, ...rest } = route;
+//             return <Route key={index} {...rest} element={element} />;
+//           })}
+//         </Routes>
+//       </Layout>
+//     );
+//   }
+// }
