17601e94d91003bba71eb2bc17c2c58e443e7176
Jehee Cheon <jeheecheon@gmail.com>
2023-08-25 09:24:31 +0900

    Create component that returns crud endpoints

--- src/react-client/src/App.tsx
+++ src/react-client/src/App.tsx
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
+import Constants from "./utilities/Constants";
+
+type Post = {
+  postId: string;
+  title: string;
+  content: string;
+}
 
 export default function App() {
   const [posts, setPosts] = useState([]);
   function getPosts() {
-    const url = 'http://localhost:5215/get-all-posts'
+    const url = Constants.API_URL_GET_ALL_POSTS;
 
     fetch(url, {
       method: 'GET'
     })
       .then(response => response.json())
       .then(postsFromServer => {
-        console.log(postsFromServer);
         setPosts(postsFromServer);
       })
       .catch((error) => {
         console.log(error);
         alert(error);
       });
-
 
   }
   return (
@@ -30,7 +35,7 @@ export default function App() {
             <button onClick={getPosts} className="btn btn-dark btn-lg  w-100">Get Posts from server</button>
             <button onClick={() => { }} className="btn btn-secondary btn-lg mt-4 w-100">Create New Post</button>
           </div>
-          {renderPostsTable()}
+          {posts.length > 0 && renderPostsTable()}
         </div>
       </div>
     </div>
@@ -50,17 +55,20 @@ export default function App() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td> Post 1 title</td>
-                <td> Post 1 content</td>
-                <td>
-                  <button className="btn btn-dark btn-lg mx-3 my-3">Update</button>
-                  <button className="btn btn-dark btn-lg mx-3 my-3">Delete</button>
-                </td>
-              </tr>
+              {posts.map((post: Post) => (
+                <tr key={post.postId}>
+                  <th scope="row">{post.postId}</th>
+                  <td>{post.title}</td>
+                  <td>{post.content}</td>
+                  <td>
+                    <button className="btn btn-dark btn-lg mx-3 my-3">Update</button>
+                    <button className="btn btn-dark btn-lg mx-3 my-3">Delete</button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
+          <button onClick={() => setPosts([])} className="btn btn-dark btn-lg w-100">Clear posts</button>
         </div>
       </>
     )
