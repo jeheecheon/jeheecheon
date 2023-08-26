e9d6cf22b11760ff55855966423080b2c2bb7c28
Jehee Cheon <jeheecheon@gmail.com>
2023-08-26 20:59:52 +0900

    Implemented functionality to Update and Delete buttons

--- src/react-client/src/App.tsx
+++ src/react-client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Constants from "./utilities/Constants";
 import PostCreateForm from "./components/PostCreateForm";
+import PostUpdateForm from "./components/PostUpdateForm";
 
 export interface Post {
   postId: number;
@@ -9,8 +10,9 @@ export interface Post {
 }
 
 export default function App() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
   const [showingCreatedNewPostForm, setShowingCreatedNewPostForm] = useState(false);
+  const [postCurrentlyBeingUpdated, setPostCurrentlyBeingUpdated] = useState<Post | null>(null);
 
   function getPosts() {
     const url = Constants.API_URL_GET_ALL_POSTS;
@@ -28,11 +30,28 @@ export default function App() {
       });
   }
 
+  function deletePost(postId: number) {
+    const url = `${Constants.API_URL_DELETE_POST}/${postId}`;
+
+    fetch(url, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+      .then(responseFromServer => {
+        console.log(responseFromServer);
+        onPostDeleted(postId);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }
+
   return (
     <div className="center">
       <div className="row min-vh-100">
         <div className="col d-flex flex-column justify-content-center align-items-center ">
-          {showingCreatedNewPostForm === false && (
+          {((showingCreatedNewPostForm === false) && (postCurrentlyBeingUpdated === null)) && (
             <>
               <h1>ASP.Net Server Test</h1>
 
@@ -43,9 +62,9 @@ export default function App() {
             </>
           )}
 
-
-          {(posts.length > 0 && !showingCreatedNewPostForm) && renderPostsTable()}
-          {showingCreatedNewPostForm && (<PostCreateForm onPostCreated={onPostCreated} />) }
+          {(posts.length > 0 && !showingCreatedNewPostForm && postCurrentlyBeingUpdated === null) && renderPostsTable()}
+          {showingCreatedNewPostForm && (<PostCreateForm onPostCreated={onPostCreated} />)}
+          {postCurrentlyBeingUpdated !== null && (<PostUpdateForm post={postCurrentlyBeingUpdated} onPostUpdated={onPostUpdated} />)}
         </div>
       </div>
     </div>
@@ -53,34 +72,38 @@ export default function App() {
 
   function renderPostsTable() {
     return (
-      <>
-        <div className="table-responsive mt-5">
-          <table className="table table-bordered border-dark">
-            <thead>
-              <tr>
-                <th scope="col">PostId (OK)</th>
-                <th scope="col">Title</th>
-                <th scope="col">Content</th>
-                <th scope="col">CRUD Operations</th>
+      <div className="table-responsive mt-5">
+        <table className="table table-bordered border-dark">
+          <thead>
+            <tr>
+              <th scope="col">PostId (OK)</th>
+              <th scope="col">Title</th>
+              <th scope="col">Content</th>
+              <th scope="col">CRUD Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post: Post) => (
+              <tr key={post.postId}>
+                <th scope="row">{post.postId}</th>
+                <td>{post.title}</td>
+                <td>{post.content}</td>
+                <td>
+                  <button onClick={() => setPostCurrentlyBeingUpdated(post)} className="btn btn-dark btn-lg mx-3 my-3">Update</button>
+                  <button onClick={() => {
+                    if (window.confirm(`Are you sure you want to delete the post titled "${post.title}?`))
+                      deletePost(post.postId);
+                  }}
+                    className="btn btn-dark btn-lg mx-3 my-3">
+                    Delete
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {posts.map((post: Post) => (
-                <tr key={post.postId}>
-                  <th scope="row">{post.postId}</th>
-                  <td>{post.title}</td>
-                  <td>{post.content}</td>
-                  <td>
-                    <button className="btn btn-dark btn-lg mx-3 my-3">Update</button>
-                    <button className="btn btn-dark btn-lg mx-3 my-3">Delete</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <button onClick={() => setPosts([])} className="btn btn-dark btn-lg w-100">Clear posts</button>
-        </div>
-      </>
+            ))}
+          </tbody>
+        </table>
+        <button onClick={() => setPosts([])} className="btn btn-dark btn-lg w-100">Clear posts</button>
+      </div>
     )
   }
 
@@ -92,5 +115,37 @@ export default function App() {
     alert(`Post successfully created. After clicking OK, your new post titled "${createdPost.title}" whill show up in the table below`);
 
     getPosts();
+  }
+
+  function onPostUpdated(updatedPost: Post | null) {
+    setPostCurrentlyBeingUpdated(null);
+
+    if (updatedPost === null) return;
+
+    let postsCopy = [...posts];
+    const index = postsCopy.findIndex((post, i) => {
+      if (post.postId === updatedPost.postId)
+        return true;
+    });
+
+    if (index !== -1)
+      postsCopy[index] = updatedPost;
+
+    setPosts(postsCopy);
+    alert(`Post successfully updated. After clicking OK, look for the post with the title "${updatedPost.postId}" in the table below to see the update`)
+  }
+
+  function onPostDeleted(deletedPostId: number) {
+    let postsCopy = [...posts];
+    const index = postsCopy.findIndex((post, i) => {
+      if (post.postId === deletedPostId)
+        return true;
+    });
+
+    if (index !== -1)
+      postsCopy.splice(index, 1);
+
+    setPosts(postsCopy);
+    alert('Post successfuly deleted. After clicking OK, look at the table below toe see your post disappear.');
   }
 }
