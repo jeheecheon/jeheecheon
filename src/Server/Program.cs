d02aabb58b7d378aa907548094d2cee85779d1d1
Jehee Cheon <jeheecheon@gmail.com>
2023-08-26 18:36:15 +0900

    Change cors adderess for react client

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options => {
     options.AddPolicy(CORSPOLICY, builder => {
         builder.AllowAnyHeader()
             .AllowAnyMethod()
-            .WithOrigins("http://localhost:3000");
+            .WithOrigins("http://localhost:44495");
     });
 });
 
