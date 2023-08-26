d642ee590c8f7a3b5f83ef8c1e852c34ec108a07
Jehee Cheon <jeheecheon@gmail.com>
2023-08-26 20:10:18 +0900

    Fixed a bug making the create post form narrow

--- src/react-client/src/components/PostCreateForm.tsx
+++ src/react-client/src/components/PostCreateForm.tsx
@@ -51,24 +51,22 @@ export default function PostCreateForm({ onPostCreated }: Props) {
   }
 
   return (
-    <div>
-      <form className='w-100 px-5'>
-        <h1 className='mt-5'>Create new post</h1>
+    <form className='w-100 px-5'>
+      <h1 className='mt-5'>Create new post</h1>
 
-        <div className="mt-5">
-          <label className='h3 form-label'>Post title</label>
-          <input value={formData.title} name='title' type='text' className='form-control' onChange={handleChange} />
-        </div>
-        <div className="mt-4">
-          <label className='h3 form-label'>Post content</label>
-          <input value={formData.content} name='content' type='text' className='form-control' onChange={handleChange} />
-        </div>
+      <div className="mt-5">
+        <label className='h3 form-label'>Post title</label>
+        <input value={formData.title} name='title' type='text' className='form-control' onChange={handleChange} />
+      </div>
+      <div className="mt-4">
+        <label className='h3 form-label'>Post content</label>
+        <input value={formData.content} name='content' type='text' className='form-control' onChange={handleChange} />
+      </div>
 
-        <button onClick={handleSubmit} className='btn btn-dark btn-lg w-100 mt-5'>Submit</button>
-        <button onClick={() => onPostCreated(null)} className='btn btn-secondary btn-lg w-100 mt-3'>Cancel</button>
+      <button onClick={handleSubmit} className='btn btn-dark btn-lg w-100 mt-5'>Submit</button>
+      <button onClick={() => onPostCreated(null)} className='btn btn-secondary btn-lg w-100 mt-3'>Cancel</button>
 
-      </form>
-    </div>
+    </form>
   );
 }
 
