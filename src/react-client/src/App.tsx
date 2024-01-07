68077f8a68287149e80bc2153ab64c2616cd2977
Jehee Cheon <jeheecheon@gmail.com>
2024-01-08 02:02:15 +0900

    Installed package react-app-aplias

--- src/react-client/src/App.tsx
+++ src/react-client/src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './App.css';
+import TestComponent from '@common/TestComponent';
 
 function App() {
+
   return (
     <React.Fragment>
-      
+      <h1 className='text-3xl font-bold underline'>
+        Hello World!
+      </h1>
+      <TestComponent></TestComponent>
     </React.Fragment>
   );
 }
